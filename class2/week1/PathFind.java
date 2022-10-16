20bae69eb67fe6c6c720d739ed506b1f746f30ed
chaerlo127 <monynony0203@gmail.com>
2022-10-16 14:37:07 +0900

    Pebble Iteration Algorithm

--- class2/week1/PathFind.java
+++ class2/week1/PathFind.java
@@ -3,45 +3,50 @@ package class2.week1;
 public class PathFind {
 	int [][] maze, memo;
 	int count, mSize;
+	
 	public PathFind(int[][] input) {
 		maze = input;
 		count = 0;
 		mSize = maze.length;
 		memo = new int[mSize][mSize];
 	}
+	
+	// bottom - up 방식
 	public int maxPathRec(int row, int col) {
 		count++;
 		if(row == 0 && col == 0)
 			return maze[row][col];
-		else if(row == 0) {
+		else if(row == 0) 
 			return maze[row][col] + maxPathRec(row, col-1);
-		}else if(col == 0)
+		else if(col == 0)
 			return maze[row][col] + maxPathRec(row-1, col);
 		else 
 			return maze[row][col]
 					+ Math.max(maxPathRec(row, col-1), maxPathRec(row-1, col));
 	}
+	
+	// top - down 방식 
 	public int maxPathIter(int row, int col) {
 		// 숙제
 		for(int i = 0 ; i<=row; i++) {
 			count++;
 			if(i == 0) memo[i][0] = maze[i][0];
-			else memo[i][0] = maze[i][0] + maze[i-1][0];
+			else memo[i][0] = maze[i][0] + maze[i-1][0]; // 가로 줄 쭉 더하기
 		}
 		for(int i = 0 ; i<=col; i++) {
 			count++;
 			if(i == 0) memo[0][i] = maze[0][i];
-			else memo[0][i] = maze[0][i] + maze[0][i-1];
+			else memo[0][i] = maze[0][i] + maze[0][i-1]; // 세로 줄 쭉 더하기
 		}
 		for(int i = 1; i<=row; i++) {
 			for(int j = 1; j<=col; j++) {
 				count++;
-				memo[i][j] = maze[i][j] + Math.max(memo[i-1][j], memo[i][j-1]);
+				memo[i][j] = maze[i][j] + Math.max(memo[i-1][j], memo[i][j-1]); // maze + 가로 세로 더한 memo array 중에서 큰 값 더하기
 			}
 		}
-		
 		return memo[row][col];
 	}
+	
 	public void reset() {
 		count = 0;
 		for(int i = 0; i<mSize; i++) {
@@ -50,28 +55,32 @@ public class PathFind {
 			}
 		}
 	}
+	
 	public int getCount() {
 		return count;
 	}
+	
+	// recursion이므로 bottom - up 방식 사용
 	public int maxpathDP(int row, int col) {
 		count++;
-		if(memo[row][col] != 0) {
+		if(memo[row][col] != 0) { // 값을 가지고 있다면
 			return memo[row][col];
 		}
 		else {
-			if(row == 0 && col == 0)
+			if(row == 0 && col == 0) 
 				memo[row][col] = maze[row][col];
-			else if(row == 0) {
+			else if(row == 0) // row는 변함 없고 col을 하나 줄여서 return -> bottom - up 방식 사용
 				memo[row][col] = maze[row][col] + maxpathDP(row, col-1);
-			}else if(col == 0)
+			else if(col == 0) // col은 변함 없고 row를 하나 줄여서 return -> bottom - up 방식 사용
 				memo[row][col] = maze[row][col] + maxpathDP(row-1, col);
-			else 
+			else
 				memo[row][col] = maze[row][col]
 						+ Math.max(maxpathDP(row, col-1), maxpathDP(row-1, col));
 			return memo[row][col];
 		}
 		
 	}
+	
 	public static void main(String[] args) {
 		int nDim = 4;
 		int[][] matrix = {{6, 7, 12, 5},
@@ -81,8 +90,8 @@ public class PathFind {
 		PathFind pf = new PathFind(matrix);
 		System.out.println("Recursion : " + pf.maxPathRec(nDim-1, nDim-1) + "(" + pf.getCount() + ")");
 		pf.reset();
-		System.out.println("Recursion : " + pf.maxPathIter(nDim-1, nDim-1) + "(" + pf.getCount() + ")");
+		System.out.println("Iteration : " + pf.maxPathIter(nDim-1, nDim-1) + "(" + pf.getCount() + ")");
 		pf.reset();
-		System.out.println("Recursion : " + pf.maxpathDP(nDim-1, nDim-1) + "(" + pf.getCount() + ")");
+		System.out.println("Dynamic Programming : " + pf.maxpathDP(nDim-1, nDim-1) + "(" + pf.getCount() + ")");
 	}
 }
--- class2/week1/Pebbles.java
+++ class2/week1/Pebbles.java
@@ -17,6 +17,7 @@ public class Pebbles {
 		nCols = peb[0].length;
 		memo = new int[5][nCols];
 	}
+	
 	void reset() {
 		count = 0;
 		for(int mi = 0; mi<5; mi++) {
@@ -25,9 +26,11 @@ public class Pebbles {
 			}
 		}
 	}
+	
 	int getCount() {
 		return count;
 	}
+	
 	public int maxPebble(int n) {
 		int max = -99999;
 		for(int j = 1; j<=4; j++) {
@@ -43,9 +46,52 @@ public class Pebbles {
 		}
 		return max;
 	}
+	
+
+	private int maxPebbleIter(int n) {
+		int max = -99999;
+		for(int j = 1; j<=4; j++) {
+			max = Math.max(pebbleIter(n, j), max);
+		}
+		return max;
+	}
+	
+	// i = 2, p = 1
+	private int pebbleIter(int i, int p) {
+		if(i == 1) {
+			memo[p][i] = aPatternValue(i, p);
+			count++;
+			return memo[p][i];
+		}
+		
+		for(int j = 1; j<=4; j++) {
+			memo[j][1] = aPatternValue(1, j);
+		}
+		
+		for(int j = 2; j<=i ; j++) { // 가로 늘리기
+			for(int a = 1; a<=4; a++) { 
+				int k = 0;
+				int max = -99999;
+				while(k<3&&nextPattern[a][k]!=0) {
+					count++;
+					int q = nextPattern[a][k];
+					
+					max =  Math.max(memo[q][j-1], max);
+					k++;
+				}
+				memo[a][j] = aPatternValue(j, a) + max;
+			}
+		}
+		int max2 = memo[1][i];
+		for(int j = 2; j<=4;j++) {
+			max2 = Math.max(memo[j][i], max2);
+		}
+		return max2; 
+	}
+	
 	private int pebbleDP(int i, int p) {
 		count++;
-		if(memo[p][i]>-99999) {
+		if(memo[p][i]>-99999) {    
 			return memo[p][i];
 		}
 		if(i == 1) {
@@ -66,7 +112,9 @@ public class Pebbles {
 			return memo[p][i];
 		}
 	}
-	int pebble(int i, int p) {
+	
+	// i는 가로, p는 세로 
+	private int pebble(int i, int p) {
 		count++;
 		if(i == 1) {
 			return aPatternValue(i, p);
@@ -81,16 +129,18 @@ public class Pebbles {
 			return aPatternValue(i, p) + max;
 		}
 	}
+	
+	// p가 세로, i가 가로 
 	private int aPatternValue(int i, int p) {
 		int retVal = 0;
 		switch(p) {
-		case 1: retVal = peb[1][i];
+		case 1: retVal = peb[1][i]; // 첫 번째 패턴
 		break;
-		case 2: retVal = peb[2][i];
+		case 2: retVal = peb[2][i]; // 두 번째 패턴
 		break;
-		case 3: retVal = peb[3][i];
+		case 3: retVal = peb[3][i]; // 세 번째 패턴
 		break;
-		case 4: retVal = peb[1][i] + peb[3][i];
+		case 4: retVal = peb[1][i] + peb[3][i]; // 네 번째 패턴
 		break;
 		}
 		return retVal;
@@ -103,14 +153,13 @@ public class Pebbles {
 			{0, -8, 10, 14, 9, 7, 13, 8, 5, 6, 1, 3, 9},
 			{0, 11, 12, 7, 4, 8, -2, 9, 4, -4, 3, 7, 10}};
 		Pebbles myPeb = new Pebbles(input);
-		
 		for(int i = 1; i<input[0].length ; i++) {
 			myPeb.reset();
 			System.out.printf(">>> %3d : [Recursion] %3d Count = %-10d", i, myPeb.maxPebble(i), myPeb.getCount());
 			myPeb.reset();
-			System.out.printf("  >>> %3d : [DynamicProg.] %3d Count = %-10d%n", i, myPeb.maxPebbleDP(i), myPeb.getCount());
+			System.out.printf("  >>> %3d : [DynamicProg.] %3d Count = %-10d", i, myPeb.maxPebbleDP(i), myPeb.getCount());
+			myPeb.reset();
+			System.out.printf("  >>> %3d : [Iteration] %3d Count = %-10d%n", i, myPeb.maxPebbleIter(i), myPeb.getCount());
 		}
 	}
-		
-
 }
